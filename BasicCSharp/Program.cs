8010c5ba87e5a71fd64ce9db5e979eabfc796b53
w4dd325 <w4dd325@gmail.com>
2021-09-29 23:36:30 +0100

    Inputs, outputs and calulations

--- BasicCSharp/Program.cs
+++ BasicCSharp/Program.cs
@@ -6,11 +6,36 @@ namespace BasicCSharp
     {
         static void Main(string[] args)
         {
+            /* 
+            //*****************************************************************************************
+            //USER INPUT AND CONSOLE OUTPUT
+            //Variables
             string yourName;
             string myName = "James";
-            Console.WriteLine("What is your name?"); 
+            int yourAge;
+            int myAge = 1000;
+
+            //Names
+            Console.WriteLine("What is your name?");
             yourName = Console.ReadLine();
             Console.WriteLine("Your name is: {0} and mine is: {1}", yourName, myName);
+
+            //Ages
+            Console.WriteLine("What is your age?");
+            yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: {0} and mine is: {1}", yourAge, myAge);
+            
+            //*****************************************************************************************
+            //CALCULATE AREA OF A CIRCLE
+            const double pi = 3.14;
+            double radius;
+            double area;
+            //your code goes here
+            Console.WriteLine("What is the radius?");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = pi * radius * radius;
+            Console.WriteLine("Area = {0}", area);
+            */
         }
     }
 }
