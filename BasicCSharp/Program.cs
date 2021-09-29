62951ff9959f010226fa5cd71217b3f0df85feb3
James <James@DESKTOP-R5583DV>
2021-09-29 21:17:01 +0100

    Add project files.

---
+++ BasicCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName;
+            string myName = "James";
+            Console.WriteLine("What is your name?"); 
+            yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: {0} and mine is: {1}", yourName, myName);
+        }
+    }
+}
